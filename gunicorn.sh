ae912e5c40bf360fede10a672ac2978aa59c800f
Iñaki Silanes <isilanes@gmail.com>
2017-12-19 11:23:06 +0100

    fix gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,4 +2,4 @@ GUNICORN=$HOME/local/python/virtualenv/django-progress/bin/gunicorn
 
 # Start Gunicorn server:
 echo Starting Gunicorn...
-exec $GUNICORN progress.wsgi:application --bind 0.0.0.0:8081 --workers 3
+exec $GUNICORN WebProgress.wsgi:application --bind 0.0.0.0:8081 --workers 3
