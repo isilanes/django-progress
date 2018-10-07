b462ae9b4937eaaaa8b8a30acc7aa9ff680a1915
isilanes <isilanes@gmail.com>
2018-10-07 22:13:00 +0200

    some modifications to allow free/non-free config

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,23 @@
+# Define variables:
+PORT=$1
+NAME=WebProgress
+GUNICORN=gunicorn
+DBDIR=$HOME/db/$NAME
+ACCESS_LOG=$DBDIR/access.log
+
+if [[ "x$1" == "x" ]]; then
+    PORT=8081
+fi
+
+# Monitor log on screen:
+tail -n 0 -f $ACCESS_LOG &
+
 # Start Gunicorn server:
 echo Starting Gunicorn...
-exec gunicorn WebProgress.wsgi:application --bind 0.0.0.0:8081 --workers 3
+exec $GUNICORN $NAME.wsgi:application \
+    --name $NAME \
+    --bind 0.0.0.0:$PORT \
+    --workers 3 \
+    --log-level=info \
+    --log-file=$DBDIR/gunicorn.log \
+    --access-logfile=$ACCESS_LOG
