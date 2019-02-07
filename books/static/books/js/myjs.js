274c49e6e025bb6c93c837e4119e155a1c3442d5
isilanes <isilanes@gmail.com>
2019-02-07 20:51:31 +0100

    books: add static dir

---
+++ books/static/books/js/myjs.js
@@ -0,0 +1,8 @@
+function mytoggle(id) {
+    var x = document.getElementById(id);
+    if (x.style.display == "none") {
+        x.style.display = "";
+    } else {
+        x.style.display = "none";
+    }
+}
