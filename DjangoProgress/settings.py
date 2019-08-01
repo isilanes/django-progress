c25f62ebd635894eb89e66b2a1243df4537c694b
isilanes <isilanes@gmail.com>
2019-08-01 21:02:33 +0200

    do some debugging

--- DjangoProgress/settings.py
+++ DjangoProgress/settings.py
@@ -128,6 +128,6 @@ STATICFILES_DIRS = [
 ]
 
 # Heroku: Update database configuration from $DATABASE_URL.
-#import dj_database_url
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
